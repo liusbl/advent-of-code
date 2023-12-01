bce88a2e53d3bc4b011fe5e3049cb12e83e36deb
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-01 23:07:57 +0200

    Create refined solution for day 1 part 2

--- src/jvmMain/kotlin/day01/refined/Day01.kt
+++ src/jvmMain/kotlin/day01/refined/Day01.kt
@@ -1,9 +1,11 @@
 package day01.refined
 
+import day01.util.indexOfOrNull
 import java.io.File
 
 fun main() {
-    solvePart1() // Solution: 55816
+//    solvePart1() // Solution: 55816
+    solvePart2() // Solution: 54980
 }
 
 fun solvePart1() {
@@ -15,3 +17,36 @@ fun solvePart1() {
     }
     println(result)
 }
+
+fun solvePart2() {
+//    val input = File("src/jvmMain/kotlin/day01/input/input_part2_test.txt")
+    val input = File("src/jvmMain/kotlin/day01/input/input.txt")
+    val lines = input.readLines()
+
+    val result = lines.sumOf { line ->
+        val firstNumber = line.firstOfAny(allNumbers).convertToDigit()
+        val lastNumber = line.reversed().firstOfAny(allNumbersReversed).reversed().convertToDigit()
+        10 * firstNumber + lastNumber
+    }
+
+    println(result)
+}
+
+val wordNumbers = mapOf(
+    "one" to 1, "two" to 2, "three" to 3, "four" to 4,
+    "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
+)
+val allNumbers = (wordNumbers.keys + wordNumbers.values.map { it.toString() }).toList()
+val allNumbersReversed = allNumbers.map { it.reversed() } // eno, owt, eerht, etc...
+
+fun String.firstOfAny(possibilities: List<String>): String =
+    possibilities.mapNotNull { number ->
+        val index = indexOfOrNull(number)
+        if (index == null) {
+            return@mapNotNull null
+        } else {
+            number to index
+        }
+    }.minBy { it.second }.first
+
+fun String.convertToDigit(): Int = if (this[0].isDigit()) toInt() else wordNumbers[this]!!.toInt()
