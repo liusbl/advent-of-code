e9b133d312cc283b510fa6000b6e654a54f4d8a3
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-01 23:13:13 +0200

    Add template

---
+++ src/jvmMain/kotlin/dayNN/initial/DayNN.kt
@@ -0,0 +1,16 @@
+package dayNN.initial
+
+import java.io.File
+
+fun main() {
+    solvePart1() // Solution:
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/dayNN/input/input.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
