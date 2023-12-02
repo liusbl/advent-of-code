afe7c3e38d9cc6ee5bdcd2267d198183de4f64ac
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-02 23:24:03 +0200

    Add empty Day03

---
+++ src/jvmMain/kotlin/day03/initial/Day03.kt
@@ -0,0 +1,16 @@
+package day03.initial
+
+import java.io.File
+
+fun main() {
+    solvePart1() // Solution:
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day03/input/input.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
