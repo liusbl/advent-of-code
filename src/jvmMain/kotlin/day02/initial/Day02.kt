263014a6d2802864447df96df06226fc60b68218
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-02 06:55:34 +0200

    Add empty day02

---
+++ src/jvmMain/kotlin/day02/initial/Day02.kt
@@ -0,0 +1,16 @@
+package day02.initial
+
+import java.io.File
+
+fun main() {
+    solvePart1() // Solution:
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day02/input/input_part1_test.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
