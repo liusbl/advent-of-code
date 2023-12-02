9cbfeec934d4e2d9db2c69414910515cf16a0e80
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-02 07:48:12 +0200

    Solve day 2 part 2

--- src/jvmMain/kotlin/day02/initial/Day02.kt
+++ src/jvmMain/kotlin/day02/initial/Day02.kt
@@ -3,7 +3,8 @@ package day02.initial
 import java.io.File
 
 fun main() {
-    solvePart1() // Solution:
+//    solvePart1() // Solution: 2268, solved at 07:31
+    solvePart2() // Solution: 63542, solved at 07:46
 }
 
 // max amount of: 12 red cubes, 13 green cubes, and 14 blue cubes
@@ -27,7 +28,46 @@ fun solvePart1() {
     println(result)
 }
 
-// Solving
+fun solvePart2() {
+//    val input = File("src/jvmMain/kotlin/day02/input/input.txt")
+    val input = File("src/jvmMain/kotlin/day02/input/input.txt")
+    val lines = input.readLines()
+
+    val gameList = lines.map(::Game)
+
+    val initialHighestList = listOf(
+        Cube(count = 0, color = Color.Red),
+        Cube(count = 0, color = Color.Green),
+        Cube(count = 0, color = Color.Blue)
+    )
+    val result = gameList.map { game ->
+        game.list.fold(GameWithCount(initialHighestList, game)) { acc, cubeDraw ->
+            val redCubeCount = cubeDraw.list.firstOrNull { it.color == Color.Red }?.count ?: 0
+            val greenCubeCount = cubeDraw.list.firstOrNull { it.color == Color.Green }?.count ?: 0
+            val blueCubeCount = cubeDraw.list.firstOrNull { it.color == Color.Blue }?.count ?: 0
+
+            val redMaxCount = maxOf(acc.highestCubeCount[0].count, redCubeCount)
+            val greenMaxCount = maxOf(acc.highestCubeCount[1].count, greenCubeCount)
+            val blueMaxCount = maxOf(acc.highestCubeCount[2].count, blueCubeCount)
+            acc.copy(
+                highestCubeCount = listOf(
+                    Cube(count = redMaxCount, color = Color.Red),
+                    Cube(count = greenMaxCount, color = Color.Green),
+                    Cube(count = blueMaxCount, color = Color.Blue)
+                )
+            )
+        }
+    }.sumOf {
+        it.highestCubeCount[0].count * it.highestCubeCount[1].count * it.highestCubeCount[2].count
+    }
+
+    println(result)
+}
+
+data class GameWithCount(
+    val highestCubeCount: List<Cube>, // red 23, green 54, blue 123
+    val game: Game
+)
 
 // Parsing
 fun Game(line: String): Game {
