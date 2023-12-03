dd851dfd466909d8eb647848f44836225c05ac13
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-03 23:19:25 +0200

    Add empty day 4

---
+++ src/jvmMain/kotlin/day04/initial/Day04.kt
@@ -0,0 +1,16 @@
+package day04.initial
+
+import java.io.File
+
+fun main() {
+    solvePart1() // Solution:
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day04/input/input.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
