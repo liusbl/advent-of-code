d104e6f3a722a2507cab1f61524ba482cf8d18e4
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-12-07 18:04:15 +0200

    Add day 7 part 2 experiment

--- src/jvmMain/kotlin/day07/initial/Day07.kt
+++ src/jvmMain/kotlin/day07/initial/Day07.kt
@@ -3,8 +3,30 @@ package day07.initial
 import java.io.File
 
 fun main() {
-    solvePart1() // Solution: 253910319, time: 08:02
+//    solvePart1() // Solution: 253910319, time: 08:02
+    solvePart2() // Solution: 253910319, time: 08:02
 }
+
+fun solvePart2() {
+    val input = File("src/jvmMain/kotlin/day07/input/input_part1_test.txt")
+//    val input = File("src/jvmMain/kotlin/day07/input/input.txt")
+    val lines = input.readLines()
+
+    val handList = lines.map {
+        it.split(" ")
+            .filter { it.isNotBlank() }
+    }.map { (handText, bidText) ->
+        Hand(handText, bidText)
+    }.sorted()
+
+    val message = handList.mapIndexed { index, hand ->
+        "${hand.list.map { it.char }.joinToString("")} ${hand.bid} ${(index + 1)} ${hand.type}"
+    }
+
+    println(message.joinToString("\n"))
+    println(handList.mapIndexed { index, hand -> hand.bid * (index + 1)}.sum())
+}
+
 
 // 254046151 is not right answer
 fun solvePart1() {
@@ -90,6 +112,42 @@ enum class Type(val rule: (hand: Hand) -> Boolean) {
     FiveOfAKind({ hand -> hand.list.distinct().size == 1 });
 }
 
+// Throws stack overflow for some reason
+//enum class Type(val rule: (hand: Hand) -> Boolean) {
+//    HighCard({ hand ->
+//        replaceJoker(hand).list.distinct().size == 5
+//    }),
+//    OnePair({ hand -> replaceJoker(hand).list.distinct().size == 4 }),
+//    TwoPair({ hand -> replaceJoker(hand).list.distinct().size == 3 }),
+//    ThreeOfAKind({ hand -> replaceJoker(hand).list.sortedBy { it.char }.windowed(3).any { it.distinct().size == 1 } }),
+//    FullHouse({ hand ->
+//        val replaced = replaceJoker(hand)
+//        replaced.list.distinct().size == 2 &&
+//                replaced.list.sortedBy { it.char }.windowed(3).any { it.distinct().size == 1 }
+//    }),
+//    FourOfAKind({ hand ->
+//        val replaced = replaceJoker(hand)
+//        replaced.list.distinct().size == 2 &&
+//                replaced.list.sortedBy { it.char }.windowed(4).any { it.distinct().size == 1 }}),
+//    FiveOfAKind({ hand -> replaceJoker(hand).list.distinct().size == 1 });
+//
+//
+//}
+
+fun replaceJoker(hand: Hand): Hand {
+    return if (hand.list.any { it == Card.Joker }) {
+        // Create all possible hands with Joker replaced
+        val handList = Card.values().map { possibleCard ->
+            val newList = hand.list.map { handCard -> if (handCard == Card.Joker) possibleCard else handCard }
+            Hand(newList, Type.HighCard, hand.bid).run { copy(type = Type.values().reversed().first { it.rule(hand) })}
+        }.sortedByDescending { it }[0]
+        handList
+    } else {
+        hand
+    }
+}
+
+
 fun Hand.compareByFirst(other: Hand): Int {
     val fiveOfAKind = other
     return 0
@@ -100,6 +158,7 @@ fun Hand.compareBySecond(other: Hand): Int {
 }
 
 enum class Card(val char: Char, val value: Int) {
+    Joker('J', 1),
     Two('2', 2),
     Three('3', 3),
     Four('4', 4),
@@ -109,7 +168,6 @@ enum class Card(val char: Char, val value: Int) {
     Eight('8', 8),
     Nine('9', 9),
     Ten('T', 10),
-    Jack('J', 11),
     Queen('Q', 12),
     King('K', 13),
     Ace('A', 14);
