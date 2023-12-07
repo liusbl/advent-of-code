4915565132b26054810da56fdefd63cef9f6a213
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-07 07:47:57 +0200

    Add day 7, doesn't work but not sure why

--- src/jvmMain/kotlin/day07/initial/Day07.kt
+++ src/jvmMain/kotlin/day07/initial/Day07.kt
@@ -6,11 +6,109 @@ fun main() {
     solvePart1() // Solution:
 }
 
+// 254046151 is not right answer
 fun solvePart1() {
+//    val input = File("src/jvmMain/kotlin/day07/input/input_part1_test.txt")
     val input = File("src/jvmMain/kotlin/day07/input/input.txt")
     val lines = input.readLines()
 
-    val result = "result"
+    val handList = lines.map {
+        it.split(" ")
+            .filter { it.isNotBlank() }
+    }.map { (handText, bidText) ->
+        Hand(handText, bidText)
+    }.sorted()
 
-    println(result)
+    val message = handList.mapIndexed { index, hand ->
+        "${hand.list.map { it.char }.joinToString("")} ${hand.bid} ${(index + 1)} ${hand.type}"
+    }
+
+    println(message.joinToString("\n"))
+}
+
+
+fun Hand(handText: String, bidText: String): Hand {
+    return Hand(
+        list = handText.map { handChar -> Card.values().first { it.char == handChar } },
+        type = Type.HighCard,
+        bid = bidText.toInt()
+    ).let { hand ->
+        hand.copy(type = Type.values().reversed().first { it.rule(hand) })
+    }
+}
+
+data class Hand(
+    val list: List<Card>, // 5 cards
+    val type: Type,
+    val bid: Int
+) : Comparable<Hand> {
+    override fun compareTo(other: Hand): Int {
+        val firstRuleResult = compareByFirstRule(other)
+        return if (firstRuleResult == 0) {
+            compareBySecondRule(other)
+        } else {
+            firstRuleResult
+        }
+    }
+
+    private fun compareByFirstRule(other: Hand): Int {
+        val rules = Type.values().reversed()
+        val typeFirst = rules.find { it.rule(this) }!!
+        val typeOther = rules.find { it.rule(other) }!!
+        return typeFirst.ordinal.compareTo(typeOther.ordinal)
+    }
+
+    private fun compareBySecondRule(other: Hand): Int {
+        val pairs = this.list.zip(other.list) { first, second ->
+            first to second
+        }
+
+        val pair = pairs.find { pair ->
+            pair.first != pair.second
+        } ?: error("Why are pairs the same? this hand: $this, other hand: $other, pairs: $pairs")
+
+        return if (pair.first > pair.second) {
+            1
+        } else {
+            -1
+        }
+    }
+}
+
+enum class Type(val rule: (hand: Hand) -> Boolean) {
+    HighCard({ hand -> hand.list.distinct().size == 5 }),
+    OnePair({ hand -> hand.list.distinct().size == 4 }),
+    TwoPair({ hand -> hand.list.distinct().size == 3 }),
+    ThreeOfAKind({ hand -> hand.list.sortedBy { it.char }.windowed(3).any { it.distinct().size == 1 } }),
+    FullHouse({ hand ->
+        hand.list.distinct().size == 2 &&
+                hand.list.sortedBy { it.char }.windowed(3).any { it.distinct().size == 1 }
+    }),
+    FourOfAKind({ hand -> hand.list.distinct().size == 2 }),
+    FiveOfAKind({ hand -> hand.list.distinct().size == 1 });
+}
+
+fun Hand.compareByFirst(other: Hand): Int {
+    val fiveOfAKind = other
+    return 0
+}
+
+fun Hand.compareBySecond(other: Hand): Int {
+    return 0
+}
+
+enum class Card(val char: Char, val value: Int) {
+    Two('2', 2),
+    Three('3', 3),
+    Four('4', 4),
+    Five('5', 5),
+    Six('6', 6),
+    Seven('7', 7),
+    Eight('8', 8),
+    Nine('9', 9),
+    Ten('T', 10),
+    Jack('J', 11),
+    Queen('Q', 12),
+    King('K', 13),
+    Ace('A', 14);
 }
