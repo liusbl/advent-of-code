72e9cdd660b956fe7f9273a3f7656bc9cae7013e
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-11 12:18:42 +0200

    Add Grid#columnList

--- src/jvmMain/kotlin/day11/initial/Grid.kt
+++ src/jvmMain/kotlin/day11/initial/Grid.kt
@@ -4,4 +4,6 @@ data class Grid<T>(
     val locationList: List<Location<T>>
 ) {
     val rowList: List<List<Location<T>>> = locationList.groupBy { it.row }.map { it.value }
+
+    val columnList: List<List<Location<T>>> = locationList.groupBy { it.column }.map { it.value }
 }
