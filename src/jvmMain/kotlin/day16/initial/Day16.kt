08f9ff0824b3f83744a6505a4965c014c772d70a
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2023-12-16 00:13:13 +0200

    create empty day 16

---
+++ src/jvmMain/kotlin/day16/initial/Day16.kt
@@ -0,0 +1,16 @@
+package day16.initial
+
+import java.io.File
+
+fun main() {
+    solvePart1() // Solution:
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day16/input/input_part1_test.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
