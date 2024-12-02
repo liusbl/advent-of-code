4168cebaf47dd3002ae6a9432ccf4ac4562aeb64
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-12-02 20:21:45 +0200

    Update template

--- src/jvmMain/kotlin/dayNN/initial/DayNN.kt
+++ src/jvmMain/kotlin/dayNN/initial/DayNN.kt
@@ -3,10 +3,27 @@ package dayNN.initial
 import java.io.File
 
 fun main() {
-    solvePart1() // Solution:
+    // Started:
+    // Finished:
+    // Solution:
+    solvePart1()
+
+    // Started:
+    // Finished:
+    // Solution:
+    //solvePart2()
 }
 
 fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/dayNN/input/input_part1_test.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
+
+fun solvePart2() {
     val input = File("src/jvmMain/kotlin/dayNN/input/input_part1_test.txt")
     val lines = input.readLines()
 
