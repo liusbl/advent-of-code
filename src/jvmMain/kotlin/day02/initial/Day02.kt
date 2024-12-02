e75614674513a88f1a27e6ff3f1c6d660b6dcc48
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-12-02 20:40:04 +0200

    Solve Day02 part2

--- src/jvmMain/kotlin/day02/initial/Day02.kt
+++ src/jvmMain/kotlin/day02/initial/Day02.kt
@@ -1,5 +1,7 @@
 package day02.initial
 
+import util.add
+import util.removeAt
 import java.io.File
 import kotlin.math.abs
 
@@ -7,7 +9,42 @@ fun main() {
     // Started: 2024-12-02 20:20
     // Finished: 2024-12-02 20:30
     // Solution: 202
-    solvePart1()
+    //solvePart1()
+
+    // Started: 2024-12-02 20:30
+    // Finished: 2024-12-02 20:39
+    // Solution: 271
+    solvePart2()
+}
+
+fun solvePart2() {
+    val input = File("src/jvmMain/kotlin/day02/input/input.txt")
+    val lines = input.readLines()
+
+    val count = lines.filter { line ->
+        val initialNumbers = line.split(" ").map { it.toInt() }
+        val numberLists = List(initialNumbers.size) { index ->
+            initialNumbers.removeAt(index)
+        }.add(initialNumbers)
+
+        numberLists.any { numbers ->
+            val sortedCondition = numbers.sorted() == numbers || numbers.sortedDescending() == numbers
+            if (!sortedCondition) {
+                return@any false
+            }
+
+            val differences = numbers.zipWithNext().map { abs(it.first - it.second) }
+            val limitCondition = differences.max() <= 3 && differences.min() >= 1
+
+            if (!limitCondition) {
+                return@any false
+            }
+
+            true
+        }
+    }.count()
+
+    println(count)
 }
 
 fun solvePart1() {
@@ -23,7 +60,7 @@ fun solvePart1() {
         }
 
         val differences = numbers.zipWithNext().map { abs(it.first - it.second) }
-        val limitCondition = differences.max() <= 3 &&  differences.min() >= 1
+        val limitCondition = differences.max() <= 3 && differences.min() >= 1
 
         if (!limitCondition) {
             return@filter false
