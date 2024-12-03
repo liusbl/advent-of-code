6270b258afb3a087bc08d1a41d267bb55253b5ac
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-12-03 11:42:01 +0200

    Solve Day03 part 2

--- src/jvmMain/kotlin/day03/initial/Day03.kt
+++ src/jvmMain/kotlin/day03/initial/Day03.kt
@@ -6,38 +6,71 @@ fun main() {
     // Started: 2024-12-03 11:06
     // Finished: 2024-12-03 11:22
     // Solution: 170778545
-    solvePart1()
+//    solvePart1()
 
-    // Started:
-    // Finished:
-    // Solution:
-    //solvePart2()
+    // Started: 2024-12-03 11:32
+    // Finished:  2024-12-03 11:41
+    // Solution: 82868252
+    solvePart2()
 }
 
-// 31705549: That's not the right answer; your answer is too low.
-//  Reason: I wasn't taking all the lines
-
-fun solvePart1() {
+fun solvePart2() {
     val input = File("src/jvmMain/kotlin/day03/input/input.txt")
+//    val input = File("src/jvmMain/kotlin/day03/input/input_part2_test.txt")
     val line = input.readLines().joinToString("")
 
-    val matcher = "mul\\((\\d+),(\\d+)\\)".toRegex()
+    val matcher = "(mul\\((\\d+),(\\d+)\\))|(don't\\(\\))|(do\\(\\))".toRegex()
 
     val matchList = matcher.findAll(line)
+
+    var enabled = true
+
     val result = matchList.sumOf { match ->
-        val first = match.groups[1]?.value?.toInt()!!
-        val second = match.groups[2]?.value?.toInt()!!
-        first * second
+        println("--------")
+        println("Match groups: ${match.groups}")
+        println("Match.value: ${match.value}")
+        val count = when (match.value) {
+            "do()" -> {
+                enabled = true
+                0
+            }
+            "don't()" -> {
+                enabled = false
+                0
+            }
+            else -> {
+                if (enabled) {
+                    val first = match.groups[2]?.value?.toInt()!!
+                    val second = match.groups[3]?.value?.toInt()!!
+                    first * second
+                } else {
+                    0
+                }
+            }
+        }
+        println("Match.value: ${match.value}. Count: $count")
+        count
     }
 
     println(result)
 }
 
-fun solvePart2() {
-    val input = File("src/jvmMain/kotlin/dayNN/input/input_part1_test.txt")
-    val lines = input.readLines()
+// 31705549: That's not the right answer; your answer is too low.
+//  Reason: I wasn't taking all the lines
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day03/input/input.txt")
+    val line = input.readLines().joinToString("")
 
-    val result = "result"
+    val matcher = "(mul\\((\\d+),(\\d+)\\))|(don't\\(\\))|(do\\(\\))".toRegex()
+
+    val matchList = matcher.findAll(line)
+    val result = matchList.sumOf { match ->
+//        val first = match.groups[1]?.value?.toInt()!!
+//        val second = match.groups[2]?.value?.toInt()!!
+//        first * second
+        println(match.groups)
+        1L
+    }
 
     println(result)
 }
