b1decc354bec0fdc7ff0cbaa121e08a40879fc12
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-12-12 14:40:21 +0200

    Parse Day 09

--- src/jvmMain/kotlin/day09/initial/Day09.kt
+++ src/jvmMain/kotlin/day09/initial/Day09.kt
@@ -3,7 +3,7 @@ package day09.initial
 import java.io.File
 
 fun main() {
-    // Started:
+    // Started: 2024-12-12 14:20
     // Finished:
     // Solution:
     solvePart1()
@@ -16,11 +16,34 @@ fun main() {
 
 fun solvePart1() {
     val input = File("src/jvmMain/kotlin/day09/input/input_part1_test.txt")
-    val lines = input.readLines()
+    val line = input.readLines()[0].map { it.digitToInt() }
 
-    val result = "result"
+    val parsedLine = line.foldIndexed(listOf<Block>()) { index, acc, digit ->
+        acc + if (index % 2 == 0) {
+            List(digit) { Block.File(index / 2) }
+        } else {
+            List(digit) { Block.Empty }
+        }
+    }
+
+    val result = parsedLine.toDisplayString()
 
     println(result)
+}
+
+sealed interface Block {
+    data class File(val id: Int) : Block
+
+    object Empty : Block
+}
+
+fun List<Block>.toDisplayString(): String {
+    return joinToString(separator = "", transform = { block ->
+        when(block) {
+            is Block.Empty -> "."
+            is Block.File -> "${block.id}"
+        }
+    })
 }
 
 fun solvePart2() {
