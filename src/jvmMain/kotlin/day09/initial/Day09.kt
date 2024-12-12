44e850f20c374f8e6410a5d48e0f2cff4d52898f
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-12-12 15:05:25 +0200

    Solve Day 09 part1

--- src/jvmMain/kotlin/day09/initial/Day09.kt
+++ src/jvmMain/kotlin/day09/initial/Day09.kt
@@ -4,42 +4,72 @@ import java.io.File
 
 fun main() {
     // Started: 2024-12-12 14:20
-    // Finished:
-    // Solution:
+    // Finished: 2024-12-12 15:05
+    // Solution: 6262891638328
+    // 829320760 is too low. Was using Int instead of Long
+    // 6262891638328
     solvePart1()
 
-    // Started:
+    // Started:2024-12-12 15:05
     // Finished:
     // Solution:
     //solvePart2()
 }
 
 fun solvePart1() {
-    val input = File("src/jvmMain/kotlin/day09/input/input_part1_test.txt")
+    val input = File("src/jvmMain/kotlin/day09/input/input.txt")
     val line = input.readLines()[0].map { it.digitToInt() }
 
-    val parsedLine = line.foldIndexed(listOf<Block>()) { index, acc, digit ->
+    val blocks = line.foldIndexed(listOf<Block>()) { index, acc, digit ->
         acc + if (index % 2 == 0) {
-            List(digit) { Block.File(index / 2) }
+            List(digit) { Block.File(index / 2.toLong()) }
         } else {
             List(digit) { Block.Empty }
         }
     }
 
-    val result = parsedLine.toDisplayString()
+    println(blocks.toDisplayString())
 
-    println(result)
+    val optimizedBlocks = blocks.toMutableList()
+    for ((index, block) in blocks.reversed().withIndex()) {
+        try {
+            if (block is Block.File) {
+                val newIndex = optimizedBlocks.indexOfFirst { it is Block.Empty }
+                if (newIndex > optimizedBlocks.indexOfLast { it is Block.File }) {
+                    println("Finished optimizing #1")
+                    break
+                }
+                optimizedBlocks[newIndex] = block
+                optimizedBlocks[blocks.size - index - 1] = Block.Empty
+              //  println(optimizedBlocks.toDisplayString())
+            }
+        } catch (_: Exception) {
+            println("Finished optimizing #2")
+            break
+        }
+    }
+
+    println(optimizedBlocks.toDisplayString())
+
+    val checksum = optimizedBlocks.withIndex().sumOf { (index, block) ->
+        when (block) {
+            Block.Empty -> 0
+            is Block.File -> index * block.id
+        }
+    }
+
+    println(checksum)
 }
 
 sealed interface Block {
-    data class File(val id: Int) : Block
+    data class File(val id: Long) : Block
 
     object Empty : Block
 }
 
 fun List<Block>.toDisplayString(): String {
     return joinToString(separator = "", transform = { block ->
-        when(block) {
+        when (block) {
             is Block.Empty -> "."
             is Block.File -> "${block.id}"
         }
