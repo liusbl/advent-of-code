5adba13ac1f920251992a4fd4d93e82b80388452
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2025-12-01 11:36:51 +0200

    Solve day 01 part 2

--- src/jvmMain/kotlin/day01/initial/Day01.kt
+++ src/jvmMain/kotlin/day01/initial/Day01.kt
@@ -6,12 +6,14 @@ fun main() {
     // Started: 2025-12-01 11:00
     // Finished: 2025-12-01 11:06
     // Solution: 1097
-    solvePart1()
+//    solvePart1()
 
-    // Started:
-    // Finished:
-    // Solution:
-    //solvePart2()
+    // Started: 2025-12-01 11:06
+    // Finished: 2025-12-01 11:35
+    // TOO LOW solution: 2731
+    // TOO HIGH solution: 7587
+    // Solution: 7101
+    solvePart2()
 }
 
 fun solvePart1() {
@@ -42,10 +44,69 @@ fun solvePart1() {
 }
 
 fun solvePart2() {
-    val input = File("src/jvmMain/kotlin/day02/input/input_part2_test.txt")
+    val input = File("src/jvmMain/kotlin/day01/input/input.txt")
     val lines = input.readLines()
 
-    val result = "result"
+    var code = 0
+    var sum = 10000050
+
+    lines.forEach {
+        val count = it.drop(1).toInt()
+
+        if (it.startsWith("L")) {
+            repeat(count) {
+                sum++
+                if (sum.toString().takeLast(2) == "00") {
+                    code++
+                }
+            }
+        } else {
+            repeat(count) {
+                sum--
+                if (sum.toString().takeLast(2) == "00") {
+                    code++
+                }
+            }
+        }
+    }
+
+    val result = code
+
+    println(result)
+}
+
+fun solvePart2_wrong() {
+    val input = File("src/jvmMain/kotlin/day01/input/input.txt")
+    val lines = input.readLines()
+
+    var sum = 10000050
+
+    var code = 0
+
+
+    lines.forEach {
+        val previousSum = sum
+
+        val count = it.drop(1)
+
+        if (it.startsWith("L")) {
+            sum += count.toInt()
+        } else {
+            sum -= count.toInt()
+        }
+
+        println(sum)
+
+        if (previousSum.toString().dropLast(2) != sum.toString().dropLast(2)) {
+            code++
+
+            if (count.toInt() > 99) {
+                code += count.toString().take(1).toInt()
+            }
+        }
+    }
+
+    val result = code
 
     println(result)
 }
