19ab621e8827ee77cd732ba8e47d3d498d3b3b46
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2025-12-04 13:09:49 +0200

    Create day 04 files

---
+++ src/jvmMain/kotlin/day04/initial/Day04.kt
@@ -0,0 +1,33 @@
+package day04.initial
+
+import java.io.File
+
+fun main() {
+    // Started:
+    // Finished:
+    // Solution:
+    solvePart1()
+
+    // Started:
+    // Finished:
+    // Solution:
+    //solvePart2()
+}
+
+fun solvePart1() {
+    val input = File("src/jvmMain/kotlin/day04/input/input_part1_test.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
+
+fun solvePart2() {
+    val input = File("src/jvmMain/kotlin/day04/input/input_part2_test.txt")
+    val lines = input.readLines()
+
+    val result = "result"
+
+    println(result)
+}
